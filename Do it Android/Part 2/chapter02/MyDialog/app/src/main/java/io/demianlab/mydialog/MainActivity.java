75a6a3cdb4aa17d85cecee34e3938c9687754b3d
peter0618 <cbj0618@gmail.com>
2017-01-17 01:24:12 +0900

    commit

--- Do it Android/Part 2/chapter02/MyDialog/app/src/main/java/io/demianlab/mydialog/MainActivity.java
+++ Do it Android/Part 2/chapter02/MyDialog/app/src/main/java/io/demianlab/mydialog/MainActivity.java
@@ -1,7 +1,12 @@
 package io.demianlab.mydialog;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,41 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("안내");
+        builder.setMessage("종료하시겠습니까?");
+        builder.setIcon(android.R.drawable.ic_dialog_alert);
+
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "예 버튼이 눌렸습니다.", Toast.LENGTH_SHORT).show();
+                Log.d("test", "i : " + i);
+            }
+        });
+
+        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "아니오 버튼이 눌렸습니다.", Toast.LENGTH_SHORT).show();
+                Log.d("test", "i : " + i);
+            }
+        });
+
+        builder.setNeutralButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(getApplicationContext(), "취소 버튼이 눌렸습니다.", Toast.LENGTH_SHORT).show();
+                Log.d("test", "i : " + i);
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
